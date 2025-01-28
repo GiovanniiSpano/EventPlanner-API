3e0acff77628c7fd140cb39e826abdd39e5b0088
Giovanni Spano <ilgiovannispano@gmail.com>
2025-01-28 23:05:54 +0100

    Implementazione DTO e ResponseEntity nei controller

--- src/main/java/com/project/eventplanner/controller/UserController.java
+++ src/main/java/com/project/eventplanner/controller/UserController.java
@@ -1,6 +1,8 @@
 package com.project.eventplanner.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
@@ -9,8 +11,11 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.project.eventplanner.model.User;
+import com.project.eventplanner.model.dto.UserDTO;
 import com.project.eventplanner.service.JwtService;
 import com.project.eventplanner.service.UserService;
+
+import jakarta.validation.Valid;
 
 @RestController
 public class UserController {
@@ -25,20 +30,22 @@ public class UserController {
     private AuthenticationManager authenticationManager;
 
     @PostMapping("/register")
-    public User register(@RequestBody User user) {
-        return this.service.saveUser(user);
+    public ResponseEntity<User> register(@Valid @RequestBody UserDTO user) {
+
+        return new ResponseEntity<>(this.service.saveUser(user), HttpStatus.CREATED);
+
     }
 
     @PostMapping("/login")
-    public String login(@RequestBody User user) {
+    public ResponseEntity<String> login(@Valid @RequestBody UserDTO user) {
 
         Authentication authentication = this.authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(user.getUsername(), user.getPassword()));
 
         if (authentication.isAuthenticated()) {
-            return this.jwtService.generateToken(user.getUsername());
+            return new ResponseEntity<>(this.jwtService.generateToken(user.getUsername()), HttpStatus.OK);
         } 
 
-        return "Login Failed";
+        return new ResponseEntity<>("Username o password errate", HttpStatus.FORBIDDEN);
     }
 
 }
--- src/main/java/com/project/eventplanner/service/UserService.java
+++ src/main/java/com/project/eventplanner/service/UserService.java
@@ -1,20 +1,36 @@
 package com.project.eventplanner.service;
 
+import java.util.Optional;
+
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.project.eventplanner.dao.UserRepository;
 import com.project.eventplanner.model.User;
+import com.project.eventplanner.model.dto.UserDTO;
 
 @Service
 public class UserService {
 
     @Autowired
     private UserRepository repo;
+
+    @Autowired
+    private ModelMapper modelMapper;
+
     final private BCryptPasswordEncoder encoder = new BCryptPasswordEncoder(12);
 
-    public User saveUser(User user) {
+    public User saveUser(UserDTO userDTO) {
+        Optional<User> userOptional = this.repo.findByUsername(userDTO.getUsername());
+
+        if (userOptional.isPresent()) throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Nome utente già in uso");
+
+        User user = this.modelMapper.map(userDTO, User.class);
+
         user.setPassword(encoder.encode(user.getPassword()));
         return this.repo.save(user);
     }
